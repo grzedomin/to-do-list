0813eebe86a281ac28d36c51e0c61a77eda6732a
Grzegorz Domin <grze.domin98@gmail.com>
2022-01-19 09:53:02 +0100

    added tasks and render function

--- js/script.js
+++ js/script.js
@@ -1,6 +1,38 @@
 {
-    const init = () => {
+    const tasks = [
+        {
+            content: "task1",
+            done: true,
+        },
+        {
+            content: "task2",
+            done: false,
+        }
+    ];
 
+    const render = () => {
+        
+        let htmlString = "";
+        
+        for(task of tasks) {
+            htmlString += `
+            <li>
+            ${task.content}
+            </li>
+            
+            `;
+        };
+        document.querySelector(".js-taskList").innerHTML = htmlString;
+
+    };
+    
+    const init = () => {
+        const form = document.querySelector(".js-form");
+        form.addEventListener("submit", (event) => {
+            event.preventDefault();
+
+        });
+        render();
     };
     init();
 }
